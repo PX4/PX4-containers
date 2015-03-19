1b5fbdbd3cbc4e817b8135a0baa493353d6a79a8
Andreas Antener <antener_a@gmx.ch>
2015-03-20 00:44:12 +0100

    add various python tools to main container (for plotting and also testing)

---
+++ docker/ros-indigo/px4-ros-full/scripts/install-python-tools.bash
@@ -0,0 +1,31 @@
+#!/bin/sh
+#
+# Create workspace at current location and fetch source repositories
+#
+
+# License: according to LICENSE.md in the root directory of the PX4 Firmware repository
+
+#
+# Install some Python Ubuntu packages
+#
+apt-get install python-pip python-mock python-enum34 python-protobuf python-pandas python-numpy python-jinja2
+
+#
+# Install some packages from source
+#
+mkdir -p python_src
+cd python_src
+
+# Used to create graphs for web
+git clone --branch px4_plots https://github.com/UAVenture/bearcart
+cd bearcart
+python setup.py install
+cd ..
+
+# Used to create plots from rosbags
+git clone https://github.com/aktaylor08/RosbagPandas
+cd RosbagPandas
+python setup.py install
+cd ..
+
+rm -rf python_src
