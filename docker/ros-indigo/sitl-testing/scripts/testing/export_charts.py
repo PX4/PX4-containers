064e2d3f635127883aa06a7778d4d1ec0170eaf2
Andreas Antener <antener_a@gmx.ch>
2015-03-30 00:34:03 +0200

    export KML from test bags

--- docker/ros-indigo/sitl-testing/scripts/testing/export_charts.py
+++ docker/ros-indigo/sitl-testing/scripts/testing/export_charts.py
@@ -10,6 +10,9 @@ import rosbag_pandas
 import os
 import sys
 
+# apt-get install ros-indigo-geodesy
+from geodesy import utm
+
 HTML_FILE = r'index.html'
 DATA_FILE = r'data.json'
 JS_FILE = r'rickshaw.min.js'
@@ -74,10 +77,52 @@ CHARTS = [
     },
 ]
 
+KML_SERIES = ["mavros_position_local__pose_position_x",
+              "mavros_position_local__pose_position_y",
+              "mavros_position_local__pose_position_z",
+              "px4_vehicle_local_position__x",
+              "px4_vehicle_local_position__y",
+              "px4_vehicle_local_position__z"]
+
+
+
+def kmlhead(name):
+    return '''<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
+<Document>
+    <name>%s</name>
+    <Style id="red">
+    <LineStyle>
+    <color>7f0000ff</color>
+    <width>4</width>
+    </LineStyle>
+    </Style>
+    <Placemark>
+        <name>%s</name>
+        <styleUrl>#red</styleUrl>
+        <LineString>
+            <coordinates>
+''' % (name, name)
+
+def kmltail(altmode):
+    return '''
+            </coordinates>
+            <tesselate>1</tesselate>
+            <altitudeMode>%s</altitudeMode>
+        </LineString>
+    </Placemark>
+</Document>
+</kml>
+''' % altmode
+
+
 def export(output_dir, bag_file):
     data = rosbag_pandas.bag_to_dataframe(bag_file)
     bag_name = os.path.splitext(os.path.basename(bag_file))[0]
     output_dir = os.path.abspath(output_dir)
+
+    #data = data.truncate(before="20150319 13:38:30", after="20150319 13:39:00")
+    #data = data.truncate(before="20150319 14:53:30", after="20150319 14:54:30")
 
     # for c in data.columns:
     #     print c
@@ -101,6 +146,37 @@ def export(output_dir, bag_file):
             vis.create_chart(html_path=HTML_FILE, data_path=DATA_FILE,
                              js_path=JS_FILE, css_path=CSS_FILE)
 
+    # KML
+    # Aarau BHF
+    base = utm.fromLatLong(47.3914028, 8.0509083, 407)
+    chart_df = data.filter(items=KML_SERIES).dropna(axis=0, how="all").resample('100L', how="first")
+    if chart_df.columns.size > 0:
+        path = os.path.join(output_dir, bag_name + '.kml')
+        print "Exporting KML to %s" % (path)
+
+        fid = open(path, 'w')
+        fid.write(kmlhead(bag_name))
+        for row_index, row in chart_df.iterrows():
+            x = base.easting
+            y = base.northing
+            a = base.altitude
+
+            if KML_SERIES[0] in chart_df.columns:
+                x = x + row[KML_SERIES[0]]
+                y = y + row[KML_SERIES[1]]
+                a = a + row[KML_SERIES[2]]
+
+            elif KML_SERIES[3] in chart_df.columns:
+                x = x + row[KML_SERIES[3]]
+                y = y + row[KML_SERIES[4]]
+                a = a - row[KML_SERIES[5]]
+
+            p = utm.UTMPoint(x, y, zone=base.zone, band=base.band).toMsg()
+
+            fid.write("%f,%f,%f " % (p.longitude, p.latitude, a))
+
+        fid.write(kmltail("absolute"))
+        fid.close()
 
 
 # Main function.
