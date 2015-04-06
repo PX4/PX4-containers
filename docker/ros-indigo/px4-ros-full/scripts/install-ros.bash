2c8eb7e4397aac1386ebb13773adc83ec620b46c
Andreas Antener <antener_a@gmx.ch>
2015-04-07 00:29:02 +0200

    use shadow repo for ROS install

--- docker/ros-indigo/px4-ros-full/scripts/install-ros.bash
+++ docker/ros-indigo/px4-ros-full/scripts/install-ros.bash
@@ -11,7 +11,7 @@ set -e
 
 ## add ROS repository and key
 ## install main ROS pacakges
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros-shadow-fixed/ubuntu/ trusty main" > /etc/apt/sources.list.d/ros-latest.list'
 wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
 sudo apt-get update
 sudo apt-get -y install ros-indigo-desktop-full
