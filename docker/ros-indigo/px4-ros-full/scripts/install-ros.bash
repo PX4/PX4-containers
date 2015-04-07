e6616a2673c01bd98723b178be0c32c7d829a1a1
Andreas Antener <antener_a@gmx.ch>
2015-04-07 12:06:41 +0200

    use shadow repo only for mavros, we don't want an otherwise possibly unstable SITL container

--- docker/ros-indigo/px4-ros-full/scripts/install-ros.bash
+++ docker/ros-indigo/px4-ros-full/scripts/install-ros.bash
@@ -11,7 +11,7 @@ set -e
 
 ## add ROS repository and key
 ## install main ROS pacakges
-sudo sh -c 'echo "deb http://packages.ros.org/ros-shadow-fixed/ubuntu/ trusty main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
 wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
 sudo apt-get update
 sudo apt-get -y install ros-indigo-desktop-full
@@ -22,11 +22,8 @@ rosdep update
 sudo sh -c 'echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc'
 source ~/.bashrc
 
-## get rosinstall
-sudo apt-get -y install python-rosinstall
-
-## additional dependencies
-sudo apt-get -y install ros-indigo-octomap-msgs ros-indigo-joy ros-indigo-mavros ros-indigo-mavros-extras ros-indigo-geodesy
+## get rosinstall and some additional dependencies
+sudo apt-get -y install python-rosinstall ros-indigo-octomap-msgs ros-indigo-joy ros-indigo-geodesy
 
 ## add osrf repository
 ## install drcsim
@@ -34,3 +31,8 @@ sudo sh -c 'echo "deb http://packages.osrfoundation.org/drc/ubuntu trusty main" 
 wget http://packages.osrfoundation.org/drc.key -O - | sudo apt-key add -
 sudo apt-get update
 sudo apt-get -y install drcsim
+
+## install mavros but from shadow repo to get latest version earlier
+sudo sh -c 'echo "deb http://packages.ros.org/ros-shadow-fixed/ubuntu/ trusty main" > /etc/apt/sources.list.d/ros-shadow.list'
+sudo apt-get update
+sudo apt-get -y install ros-indigo-mavros ros-indigo-mavros-extras
