64154790cc53ea598d6222d28abf36358487cc4a
Daniel Agar <daniel@agar.ca>
2019-01-26 10:39:38 -0500

    Dockerfile base create default user with UID 1001
     - update entrypoint.sh to modify the uid if LOCAL_USER_ID passed

--- docker/px4-dev/scripts/entrypoint.sh
+++ docker/px4-dev/scripts/entrypoint.sh
@@ -4,40 +4,23 @@
 # Either use the LOCAL_USER_ID if passed in at runtime or
 # fallback
 
-USER_ID=${LOCAL_USER_ID:-9001}
+if [ -n "${LOCAL_USER_ID}" ]; then
 
-echo "Starting with UID : $USER_ID"
-useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
-usermod -a -G dialout user
-usermod -a -G sudo user
+	echo "Starting with UID : $LOCAL_USER_ID"
 
-if [ -d "$CCACHE_DIR" ]; then
-	chown -R $USER_ID $CCACHE_DIR
-	if [ ! $? -eq 0 ]; then
-		echo "ccache permission issues, disabling"
-		export CCACHE_DISABLE=1
-	else
-		export CCACHE_BASEDIR=`pwd`
-		export CCACHE_UMASK=002
+	# modify existing user's id
+	usermod -u $LOCAL_USER_ID user
+
+	if [ -d "$CCACHE_DIR" ]; then
+		chown -R $USER_ID $CCACHE_DIR
 	fi
+
+	exec gosu user "$@"
+
 else
-	export CCACHE_DISABLE=1
+	if [ -d "$CCACHE_DIR" ]; then
+		chown -R $USER_ID $CCACHE_DIR
+	fi
+
+	exec "$@"
 fi
-
-# unsetting ROS_DISTRO to silence ROS_DISTRO override warning
-unset ROS_DISTRO
-
-# setup ros1 environment
-if [ -v ROS1_DISTRO ] && [ ! -z "$ROS1_DISTRO" ] && [ -f /opt/ros/$ROS1_DISTRO/setup.bash ]; then
-	source /opt/ros/$ROS1_DISTRO/setup.bash
-fi
-
-# setup ros2 environment
-if [ -v ROS2_DISTRO ] && [ ! -z "$ROS2_DISTRO" ] && [ -f /opt/ros/$ROS2_DISTRO/setup.bash ]; then
-	source /opt/ros/$ROS2_DISTRO/setup.bash
-fi
-
-unset ROS1_DISTRO
-unset ROS2_DISTRO
-
-exec gosu user "$@"
