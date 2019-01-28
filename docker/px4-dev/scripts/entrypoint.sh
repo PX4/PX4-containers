e0fe0a5b7d026fc9fde574bb31a31b018d377a89
Martina Rivizzigno <martina@rivizzigno.it>
2019-01-28 21:25:03 +0100

    entrypoint: run xvfb if ROS_DISTRO is melodic

--- docker/px4-dev/scripts/entrypoint.sh
+++ docker/px4-dev/scripts/entrypoint.sh
@@ -4,6 +4,11 @@
 # Either use the LOCAL_USER_ID if passed in at runtime or
 # fallback
 
+if [ "${ROS_DISTRO}" == "melodic" ]; then
+	export DISPLAY=:0
+	Xvfb :1 -screen 0 1600x1200x24+32 &
+fi
+
 if [ -n "${LOCAL_USER_ID}" ]; then
 
 	echo "Starting with UID : $LOCAL_USER_ID"
