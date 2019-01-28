44d44074d2b1894ff419aefd5fc3306234c9db27
Daniel Agar <daniel@agar.ca>
2019-01-28 10:07:38 -0500

    entrypoint delete broken ccache ownership change

--- docker/px4-dev/scripts/entrypoint.sh
+++ docker/px4-dev/scripts/entrypoint.sh
@@ -11,16 +11,9 @@ if [ -n "${LOCAL_USER_ID}" ]; then
 	# modify existing user's id
 	usermod -u $LOCAL_USER_ID user
 
-	if [ -d "$CCACHE_DIR" ]; then
-		chown -R $USER_ID $CCACHE_DIR
-	fi
-
 	exec gosu user "$@"
 
 else
-	if [ -d "$CCACHE_DIR" ]; then
-		chown -R $USER_ID $CCACHE_DIR
-	fi
 
 	exec "$@"
 fi
