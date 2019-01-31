dbc6a8f64eb94b7faed73add9f54ebcdd84e5c4a
Anthony Lamping <lamping.ap@gmail.com>
2019-01-31 01:02:25 -0500

    entrypoint: use Xvfb if no display

--- docker/px4-dev/scripts/entrypoint.sh
+++ docker/px4-dev/scripts/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Start virtual X server in the background if there is no display
+if [ -z "$DISPLAY" ]; then
+	echo "Starting Xvfb"
+	Xvfb :99 -screen 0 1600x1200x24+32 &
+	export DISPLAY=:99
+fi
+
 # Add local user
 # Either use the LOCAL_USER_ID if passed in at runtime or
 # fallback
