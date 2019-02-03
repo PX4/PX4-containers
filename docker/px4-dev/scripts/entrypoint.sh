31dc86bd112d3aa28ad44c6dee7f0cbd93c83728
Daniel Agar <daniel@agar.ca>
2019-02-03 15:17:43 -0500

    entrypoint.sh only start Xvfb if exists

--- docker/px4-dev/scripts/entrypoint.sh
+++ docker/px4-dev/scripts/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start virtual X server in the background if there is no display
-if [ -z "$DISPLAY" ]; then
+if [[ -x "$(command -v Xvfb)" && -z "$DISPLAY" ]]; then
 	echo "Starting Xvfb"
 	Xvfb :99 -screen 0 1600x1200x24+32 &
 	export DISPLAY=:99
